22564d58dc73b3a617de48b853c1f18d9a5fa8a3
raymond <raymond.wouapi@wouri.co>
2017-02-19 15:03:50 +0100

    remove useless dependencies

--- spark-logstash-dummy/src/main/java/com/wouri/streamly/spark/logstash/Logstash.java
+++ spark-logstash-dummy/src/main/java/com/wouri/streamly/spark/logstash/Logstash.java
@@ -1,8 +1,5 @@
 package com.wouri.streamly.spark.logstash;
 
-import javax.script.Invocable;
-import javax.script.ScriptException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,22 +7,21 @@ import org.slf4j.LoggerFactory;
  * @author Pascal Fenkam
  */
 public class Logstash {
-    private static Logstash instance;
-    static Logger logger = LoggerFactory.getLogger(Logstash.class);
+	private static Logstash instance;
+	static Logger logger = LoggerFactory.getLogger(Logstash.class);
 
-    public static Logstash start(String[] args){
-        if (instance == null){
-            instance = new Logstash(args);
-        }
-        return instance;
-    }
+	public static Logstash start(String[] args) {
+		if (instance == null) {
+			instance = new Logstash(args);
+		}
+		return instance;
+	}
 
-    private Logstash(String[] args){
+	private Logstash(String[] args) {
 
-    }	
+	}
 
-    
-    public void addToQueue(String log){
-         logger.info("Data added to logstash queue :{}",log);
-    }
+	public void addToQueue(String log) {
+		logger.info("Data added to logstash queue :{}", log);
+	}
 }
