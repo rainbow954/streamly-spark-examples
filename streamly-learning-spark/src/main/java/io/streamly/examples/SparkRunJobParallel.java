ce8851ef62556b6e57a1dc55397407944ccc8cf5
raymond <raymond.wouapi@wouri.co>
2017-05-12 16:38:46 +0100

    run two jobs in parallel

--- streamly-learning-spark/src/main/java/io/streamly/examples/SparkRunJobParallel.java
+++ streamly-learning-spark/src/main/java/io/streamly/examples/SparkRunJobParallel.java
@@ -6,35 +6,43 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkRunJobParallel {
-	public static void main(String...args) throws InterruptedException, ExecutionException {
-		final JavaSparkContext sc = new JavaSparkContext("local[2]","Simple_App");
-	    ExecutorService executorService = Executors.newFixedThreadPool(2);
-	    // Start thread 1
-	    Future<Long> future1 = executorService.submit(new Callable<Long>() {
+	public static void main(String... args) throws InterruptedException, ExecutionException {
+		// Create a Java Spark Context
+		SparkConf conf = new SparkConf()
+								.setMaster("local[2]")
+								.setAppName("Simple Application");
+
+		final JavaSparkContext sc = new JavaSparkContext(conf);
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		
+		// Start thread 1
+		Future<Long> future1 = executorService.submit(new Callable<Long>() {
 
 			@Override
 			public Long call() throws Exception {
 				JavaRDD<String> file1 = sc.textFile("README.md");
-	            return file1.count();
+				return file1.count();
 			}
 		});
-	    // Start thread 2
-	    Future<Long> future2 = executorService.submit(new Callable<Long>() {
-	        @Override
-	        public Long call() throws Exception {
-	            JavaRDD<String> file2 = sc.textFile("spark.properties");
-	            return file2.count();
-	        }
-	    });
-	    // Wait thread 1
-	    System.out.println("File1:"+future1.get());
-	    // Wait thread 2
-	    System.out.println("File2:"+future2.get());
-	    sc.close();
+		// Start thread 2
+		Future<Long> future2 = executorService.submit(new Callable<Long>() {
+			@Override
+			public Long call() throws Exception {
+				JavaRDD<String> file2 = sc.textFile("spark.properties");
+				return file2.count();
+			}
+		});
+		// Wait thread 1
+		System.out.println("File1:" + future1.get());
+		// Wait thread 2
+		System.out.println("File2:" + future2.get());
+		// Close spark context
+		sc.close();
 	}
 
 }
